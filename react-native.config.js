49195a2154d68bd229c9ad3e5738b672101600fd
Jin Shin <jinshin1013@gmail.com>
2020-04-13 22:18:39 +1000

    Add native project path to react-native config.

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  project: {
+    ios: {
+      project: './Playground/ios/Playground.xcworkspace',
+    },
+    android: {
+      sourceDir: './Playground/android',
+    },
+  },
+}
